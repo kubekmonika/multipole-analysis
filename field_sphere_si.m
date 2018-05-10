b3866ff3751a5eb15050b9057e6344d23885a399
Alexona <kubek.monika@wp.pl>
2018-05-10 15:19:33 +0200

    Poprawiono oznaczenia, dodano przeliczanie pola E na wspl sferyczne

--- field_sphere_si.m
+++ field_sphere_si.m
@@ -1,27 +1,18 @@
-function [E, R, eneis, n, theta, phi] = field_sphere_si(az_n, el_n)
-% Oblicza pole dla krzemowej sfery o srednicy 200nm, 
-% kolejno dla roznych dlugosci fali padajacej 
-% enei=(350, 950)nm co 10 nm.
+function [Esph, R, eneis, N, theta, phi] = field_sphere_si(n_theta, n_phi)
+% Oblicza pole we wspolrzednych sferycznych dla krzemowej sfery 
+% o srednicy 200nm, kolejno dla roznych dlugosci fali padajacej.
 % 
-%   FIELD_SPHERE_SI(az_n, el_n)
+%   FIELD_SPHERE_SI(n_theta, n_phi)
 % 
-%   az_n - liczba punktow dla wspolrzednej azymutalnej
-%   el_n - liczba punktow dla wspolrzednej zenitalnej
+%   n_phi - liczba punktow dla wspolrzednej azymutalnej
+%   n_theta - liczba punktow dla wspolrzednej zenitalnej
 
-% IMPORTUJEMY DLUGOSCI FALI
-% filename = '/home/monika/HDD/MATLAB/analiza_multipolowa/krzem_n.txt';
-% delimiter = '\t';
-% formatSpec = '%f%f%[^\n\r]';
-% fileID = fopen(filename,'r');
-% dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'TextType', 'string',  'ReturnOnError', false);
-% fclose(fileID);
-% eneis = dataArray{:, 1}' * 1000; %[nm]
-% n = dataArray{:, 2}';
-% clearvars filename delimiter formatSpec fileID dataArray ans;
-
-% definiujemy parametry dla osrodka w ktorym znajduje sie sfera
-eneis = linspace(450, 950, 100);
-n = ones(1, length(eneis));
+% Definiujemy parametry dla osrodka w ktorym znajduje sie sfera.
+% dlugosci fali
+eneis = linspace(450, 950, 100);  
+% relacja dyspersyjna, ogólniej n może być liczbą, 
+% ale dla ośrodków z absorpcją lub nieizotropowych metoda nie działa
+N = ones(1, length(eneis));  % wspolczynnik zalamania
 
 % TWORZYMY OBIEKTY
 global op p bem exc x y z
@@ -37,22 +28,34 @@ bem = bemsolver( p, op );
 %kierunek fali
 exc = planewave( [ 1, 0, 0 ], [ 0, 0, 1 ], op );
 % wspolrzedne w ktorych liczymy pole
-[x, y, z, ~, ~, ~, theta, phi] = sferawspl(R, az_n, el_n);
+[x, y, z, ~, ~, ~, theta, phi] = sferawspl(R, n_theta, n_phi);
 % macierz wynikowa
-E = zeros(az_n*el_n, 3, length(eneis));
+E = zeros(n_phi*n_theta, 3, length(eneis));
 
 % LICZYMY POLE
 multiWaitbar( 'Obliczanie pola', 0, 'Color', 'g', 'CanCancel', 'on' );
-N = length(eneis);
-for i = 1 : N
+n_eneis = length(eneis);
+for i = 1 : n_eneis
     E(:,:,i) = calculatefield(eneis(i));
-    multiWaitbar( 'Obliczanie pola', i / N );
+    multiWaitbar( 'Obliczanie pola', i / n_eneis );
 end
 multiWaitbar('Obliczanie pola', 'Close');
+
+% zmiana wspolrzednych E z kartezjanskich na sferyczne
+Esph = 0 * E;
+for i = 1 : size(E,3)
+    % r
+    Esph(:,1,i) = sqrt(E(:,1,i).^2 + E(:,2,i).^2 + E(:,3,i).^2);
+    % theta
+    Esph(:,2,i) = acos(E(:,3,i) ./ Esph(:,1,i));
+    % phi
+    Esph(:,3,i) = atan(E(:,2,i) ./ E(:,1,i));
+end
 end
 
 function e = calculatefield(enei)
 % Liczy pole dla zadanej dlugosci fali enei
+% Zwraca pole we współrzędnych kartezjanskich
 global op p bem exc x y z
 sig = bem \ exc( p, enei );
 emesh = meshfield( p, x, y, z, op, 'mindist', 1, 'nmax', 5000 , 'waitbar', 0);
