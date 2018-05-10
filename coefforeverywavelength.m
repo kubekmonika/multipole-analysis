98e6cc2942023192624d3ad7dedacab72e96839a
Alexona <kubek.monika@wp.pl>
2018-05-10 20:23:45 +0200

    Poprawiono oznaczenia

--- coefforeverywavelength.m
+++ coefforeverywavelength.m
@@ -1,38 +1,23 @@
-function C = coefforeverywavelength(E, R, eneis, n, theta, phi)
+function C = coefforeverywavelength(E, R, eneis, N, theta, phi)
 %Funkcja liczaca wspolczynniki rozproszenia dla danego pola.
 
 % funkcja liczaca wektor falowy: k^2 = 4 pi^2 n^2 / lambda^2
 % wavevector = @(n, enei) sqrt(4 * pi^2 * n^2 / (enei * 0.1^9)^2);
-
+% E - rozklad pola, wywolanie we wspolrzednych sferycznych!!
 % c = 299792458; %[m/s] predkosc swiatla w prozni
-wavevector = @(n, enei) 2 * pi / (enei * 0.1^9);
+% N - wspolczynnik zalamania, musi byc rzeczywisty bo innych przypadkow
+%     teoria w tej metodzie nie przewiduje
+wavevector = @(n, enei) 2 * pi * n / (enei * 0.1^9);     % KS: dopisałam mnożenie przez n
 
 % obliczamy wspolczynniki rozproszenia
-N = length(n);
-C = zeros(1, N);
-
-% pasek czasu
-% if isempty(gcp('nocreate'))
-%     parpool('local', 4);
-% end
-% dq = parallel.pool.DataQueue;
-% wb = waitbar(0, 'Calculating...');
-% wb.UserData = [0 N];
-% afterEach(dq, @(varargin) iIncrementWaitbar(wb));
+n_N = length(N);
+C = zeros(1, n_N);
 
 % petla for / parfor
-for i = 1: N
-    k = wavevector(n(i), eneis(i));
+for i = 1: n_N
+    k = wavevector(N(i), eneis(i));
     e = E(:,:,i);
     C(i) = scattcoefficienta(1, k, e, R, theta, phi);
 end
-% close(wb);
-end
 
-function iIncrementWaitbar(wb)
-% Pasek czasu dla parfor
-ud = wb.UserData;
-ud(1) = ud(1) + 1;
-waitbar(ud(1) / ud(2), wb);
-wb.UserData = ud;
 end
