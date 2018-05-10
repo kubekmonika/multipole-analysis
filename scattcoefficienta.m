711930b80a0a00cd681336d0d0dd35614fca00ff
Alexona <kubek.monika@wp.pl>
2018-05-10 20:21:56 +0200

    Poprawiono oznaczenia

--- scattcoefficienta.m
+++ scattcoefficienta.m
@@ -1,40 +1,40 @@
-function a = scattcoefficienta(n, k, e, R, theta, phi)
-%Oblicza wspolczynnik rozproszenia a(n).
+function a = scattcoefficienta(l, k, e, R, theta, phi)
+%Oblicza wspolczynnik rozproszenia a(l).
 %
-%   SCATTCOEFFICIENTA(n, m, E, R, theta, phi)
+%   SCATTCOEFFICIENTA(l, m, E, R, theta, phi)
 % 
-%   n - stopien, n >= |m|
+%   l - stopien, l >= |m|
 %   k - wartosc wektora falowego
 %   e - rozklad pola (w postaci wektora)
 %   R - promien sfery
-%   theta - katy azymutalne
 %   phi - katy zenitalne
+%   theta - katy azymutalne
 % 
 a = 0;
-for m = -n : 1 : n
-    anm = a_nm(m, n, k, e, R, theta, phi);
-    a = a + k^2 * n * (n + 1) * (anm * conj(anm));
+for m = -l : 1 : l
+    alm = a_lm(l, m, k, e, R, theta, phi);
+    a = a + k^2 * l * (l + 1) * (alm * conj(alm));
 end
 
 end
 
-function anm = a_nm(m, n, k, e, R, theta, phi)
-% Zwraca wartosc wspolczynnika rozproszenia a_nm
-assert(n >= abs(m), 'Blad: |m| > n')
+function alm = a_lm(l, m, k, e, R, theta, phi)
+% Zwraca wartosc wspolczynnika rozproszenia a_lm
+assert(l >= abs(m), 'Blad: |m| > n')
 % VSH
 % N = vsh('N', n, m, theta, phi, R, k);
 if m >= 0
-    N = vsh('N', n, m, theta, phi, R, k);
+    N = vsh('N', l, m, theta, phi, R, k);
 else
-    N = (-1)^(abs(m)) * conj(vsh('N', n, abs(m), theta, phi, R, k));
+    N = (-1)^abs(m) * conj(vsh('N', l, abs(m), theta, phi, R, k));
 end
 % przeksztalcamy pole
 E = sphreshapefield(e, length(theta), length(phi));
 % zastepujemy NaN wartoscia 0
 N(isnan(N)) = 0;
 E(isnan(E)) = 0;
-% licznik 
+% licznik
 licznik = dot(E, conj(N), 3) .* sin(theta);
 mianownik = dot(N, conj(N), 3) .* sin(theta);
-anm = sum(licznik(:)) / sum(mianownik(:));
+alm = sum(licznik(:)) / sum(mianownik(:));
 end
