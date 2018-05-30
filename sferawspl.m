3e4a89721b8d5f63ea5b70abacfe4a6212500080
Alexona <kubek.monika@wp.pl>
2018-05-30 13:40:39 +0200

    Poprawiono definicje katow

--- sferawspl.m
+++ sferawspl.m
@@ -2,7 +2,7 @@ function [x, y, z, az, el, rad, theta, phi] = sferawspl(R, n_theta, n_phi)
 % Tworzy sferę o zadanym promieniu i zwraca jej wspolrzedne
 % w ukladzie kartezjanskim i sferycznym.
 % 
-%   [x, y, z, az, el, rad, theta, phi] = SFERAWSPL(R, n_az, n_el)
+%   [x, y, z, az, el, rad, theta, phi] = SFERAWSPL(R, n_theta, n_phi)
 % 
 %   R - promien sfery
 %   n_theta - liczba punktow na jakie dzieli sie kat zenitalny
@@ -15,18 +15,33 @@ function [x, y, z, az, el, rad, theta, phi] = sferawspl(R, n_theta, n_phi)
 % wiersze - phi - az
 % kolumny - theta - el
 
+% %% wspolrzedne zanitalne
+% theta = linspace(0, 2*pi, n_theta + 1) + 0.1; 
+% theta = theta(1: n_theta);
+% az = theta .* ones(n_phi, n_theta);
+% az = az(:);
+% % wspolrzedne azymutalne
+% phi = linspace(-pi/2, pi/2, n_phi + 1) + 0.001;
+% phi = phi(1: n_phi);
+% el = phi' .* ones(n_phi, n_theta);
+% el = el(:);
+% % wspolrzedne promienia
+% rad = ones(n_phi * n_theta, 1) * R;
+% % przechodzimy do wspolrzednych kartezjanskich
+% [x, y, z] = sph2cart(az, el, rad);
+
 %% wspolrzedne zanitalne
-theta = linspace(0, 2*pi, n_theta + 1) + 0.1; 
+theta = linspace(0, pi, n_theta + 1); 
 theta = theta(1: n_theta);
-az = theta .* ones(n_phi, n_theta);
-az = az(:);
-% wspolrzedne azymutalne
-phi = linspace(-pi/2, pi/2, n_phi + 1) + 0.001;
-phi = phi(1: n_phi);
-el = phi' .* ones(n_phi, n_theta);
+el = theta .* ones(n_phi, n_theta);
 el = el(:);
+% wspolrzedne azymutalne
+phi = linspace(0, 2*pi, n_phi + 1);
+phi = phi(1: n_phi);
+az = phi' .* ones(n_phi, n_theta);
+az = az(:);
 % wspolrzedne promienia
 rad = ones(n_phi * n_theta, 1) * R;
 % przechodzimy do wspolrzednych kartezjanskich
-[x, y, z] = sph2cart(az, el, rad);
+[x, y, z] = sph2cart(az, pi/2 - el, rad);
 end
