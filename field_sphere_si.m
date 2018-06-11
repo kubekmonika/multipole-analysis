0a63f2dcec62330732b7a1510abbdee5708eabe0
Alexona <kubek.monika@wp.pl>
2018-06-11 10:43:42 +0200

    Dodano mesh

--- field_sphere_si.m
+++ field_sphere_si.m
@@ -1,18 +1,21 @@
-function [E, R, eneis, N, theta, phi] = field_sphere_si(n_theta, n_phi, dir, pol)
+function [E, R, eneis, N, x, y, z] = field_sphere_si(n_points, dir, pol)
 % Oblicza pole we wspolrzednych sferycznych dla krzemowej sfery 
 % o srednicy 200nm, kolejno dla roznych dlugosci fali padajacej.
 % 
-%   FIELD_SPHERE_SI(n_theta, n_phi)
+%   FIELD_SPHERE_SI(n_points, dir, pol)
 % 
-%   n_phi - liczba punktow dla wspolrzednej azymutalnej
-%   n_theta - liczba punktow dla wspolrzednej zenitalnej
+%   n_points - liczba punktow do triangulacji; mozliwe wartosci:
+%       32 60 144 169 225 256 289 324 361 400 441 484 529 576 625
+%       676 729 784 841 900 961 1024 1225 1444
+%   dir - kierunek fali padajacej
+%   pol - polaryzacja fali padajacej
 
 % Definiujemy parametry dla osrodka w ktorym znajduje sie sfera.
 % dlugosci fali
-eneis = linspace(450, 850, 80);  
+eneis = linspace(450, 850, 40);  
 % relacja dyspersyjna, ogólniej n może być liczbą, 
 % ale dla ośrodków z absorpcją lub nieizotropowych metoda nie działa
-N = ones(1, length(eneis));  % wspolczynnik zalamania
+N = ones(length(eneis), 1);  % wspolczynnik zalamania
 
 % % kierunek fali
 % dir = [ 0, 0, 1 ];
@@ -20,39 +23,42 @@ N = ones(1, length(eneis));  % wspolczynnik zalamania
 % pol = [ 0, 1, 0 ];
 
 % TWORZYMY OBIEKTY
-global op p bem exc x y z
+global op p bem exc
 op = bemoptions( 'sim', 'ret', 'interp', 'curv', 'refine', 2 );
 epstab = { epsconst( 1 ), epstable( 'silicon2.dat' ) };
 %  nanosfera
 p = trisphere( 144, 200 );
 p = comparticle( epstab, { p }, [ 2, 1 ], 1, op );
-% promien otaczajcej sfery
+% promien otaczającej sfery
 R = 105;
 % solver
 bem = bemsolver( p, op );
 %kierunek fali
 exc = planewave( dir, pol, op );
 % wspolrzedne w ktorych liczymy pole
-[x, y, z, ~, ~, ~, theta, phi] = sferawspl(R, n_theta, n_phi);
+sphere = trisphere(n_points, 2*R);
+x = sphere.verts(:,1);
+y = sphere.verts(:,2);
+z = sphere.verts(:,3);
 % macierz wynikowa
-E = zeros(n_phi*n_theta, 3, length(eneis));
+E = zeros(n_points, 3, length(eneis));
 
 % LICZYMY POLE
 multiWaitbar( 'Obliczanie pola', 0, 'Color', 'g', 'CanCancel', 'on' );
 n_eneis = length(eneis);
 for i = 1 : n_eneis
-    E(:,:,i) = calculatefield(eneis(i));
+    E(:,:,i) = calculatefield(eneis(i), x, y, z);
     multiWaitbar( 'Obliczanie pola', i / n_eneis );
 end
 multiWaitbar('Obliczanie pola', 'Close');
 
 end
 
-function e = calculatefield(enei)
+function e = calculatefield(enei, x, y, z)
 % Liczy pole dla zadanej dlugosci fali enei
 % Zwraca pole we współrzędnych kartezjanskich
-global op p bem exc x y z
+global op p bem exc
 sig = bem \ exc( p, enei );
-emesh = meshfield( p, x, y, z, op, 'mindist', 1, 'nmax', 1200 , 'waitbar', 0);
+emesh = meshfield( p, x, y, z, op, 'mindist', 2, 'nmax', 1200 , 'waitbar', 0);
 e = emesh( sig ) ; 
 end
